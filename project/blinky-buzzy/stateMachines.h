d7a5eb4814febf85475d4dab03588e35e3b5d1e6
Josue Morales <jmorales41@miners.utep.edu>
2021-04-09 11:04:56 -0600

    Added state machine code that is in charge storing the different states the msp430 will have

---
+++ project/blinky-buzzy/stateMachines.h
@@ -0,0 +1,19 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+
+void sm_slow_clock();
+void sm_fast_clock();
+void dim_lights();
+
+void stop();
+void play_tone();
+void state_blink();
+
+char toggle_red();
+
+char toggle_green();
+
+
+
+#endif // included
