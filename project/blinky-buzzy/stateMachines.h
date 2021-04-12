bb862129c18563aa2a2db24fd35ccebdf44e5793
josu-ke <jmorales5845@gmail.com>
2021-04-12 00:30:27 -0600

    Updated stateMachines.h

--- project/blinky-buzzy/stateMachines.h
+++ project/blinky-buzzy/stateMachines.h
@@ -2,14 +2,11 @@
 #define stateMachine_included
 
 
-void sm_slow_clock();
-void sm_fast_clock();
-void dim_lights();
 
 void stop();
 void play_tone();
 void state_blink();
-
+void play_another_tone();
 char toggle_red();
 
 char toggle_green();
