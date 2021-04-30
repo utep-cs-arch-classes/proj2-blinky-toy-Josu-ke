ce4227cdea403dd1c5ea7501a0fe558e63fe5693
josu-ke <jmorales5845@gmail.com>
2021-04-30 14:15:18 -0600

    Added shape for milestone demo and its libraries

--- project/blinky-buzzy/blinkBuzzMain.c
+++ project/blinky-buzzy/blinkBuzzMain.c
@@ -3,14 +3,31 @@
 #include "led.h"
 #include "switches.h"
 #include "buzzer.h"
+#include "lcdutils.h"
+#include "lcddraw.h"
 
 int main(void) {
   configureClocks();
   buzzer_init();
   switch_init();
   led_init();
+  lcd_init();
   
   enableWDTInterrupts();
+
+  
+
+char width = screenWidth;
+char height = screenHeight;
+
+clearScreen(COLOR_BLACK);
+drawPixel(height/2,width/2,COLOR_RED);
+  for(int i=0; i< 20;i++){
+    for(int j=0; j<=i;j++){
+	
+	drawPixel(50+i,50+j,COLOR_WHITE);
+      }
+  }
 
 
   or_sr(0x18);
